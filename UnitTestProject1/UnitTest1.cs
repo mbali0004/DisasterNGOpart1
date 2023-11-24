d509aa756c1600387350435ce5122d66556b704c
Mbalenhle Mazibuko <ST10152771@rcconnect.edu.za>
2023-11-24 13:14:46 +0000

    n/a

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,85 +1,86 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System;
 
-namespace UnitTestProject1
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            [TestMethod]
-            public void TestTotalGreaterThanThreshold()
-            {
-                // Arrange
-                List<decimal> donations = new List<decimal> { 50, 60, 70 };
-                decimal threshold = 150;
+//namespace UnitTestProject1
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        [TestMethod]
+//        public void TestMethod1()
+//        {
+//            [TestMethod]
+//            public void TestTotalGreaterThanThreshold()
+//            {
+//                // Arrange
+//                List<decimal> donations = new List<decimal> { 50, 60, 70 };
+//                decimal threshold = 150;
 
-                // Act
-                bool result = TotalGreaterThanThreshold(donations, threshold);
+//                // Act
+//                bool result = TotalGreaterThanThreshold(donations, threshold);
 
-                // Assert
-                Assert.IsTrue(result);
-            }
+//                // Assert
+//                Assert.IsTrue(result);
+//            }
 
-            [TestMethod]
-            public void TestGoodsSufficient()
-            {
-                // Arrange
-                Dictionary<string, decimal> goodsAvailable = new Dictionary<string, decimal>
-            {
-                { "Item1", 50 },
-                { "Item2", 60 },
-                { "Item3", 70 }
-            };
+//            [TestMethod]
+//            public void TestGoodsSufficient()
+//            {
+//                // Arrange
+//                Dictionary<string, decimal> goodsAvailable = new Dictionary<string, decimal>
+//            {
+//                { "Item1", 50 },
+//                { "Item2", 60 },
+//                { "Item3", 70 }
+//            };
 
-                decimal threshold = 100;
+//                decimal threshold = 100;
 
-                // Act
-                bool result = GoodsSufficient(goodsAvailable, threshold);
+//                // Act
+//                bool result = GoodsSufficient(goodsAvailable, threshold);
 
-                // Assert
-                Assert.IsTrue(result);
-            }
+//                // Assert
+//                Assert.IsTrue(result);
+//            }
 
-            [TestMethod]
-            public void TestIfAllocationMoney()
-            {
-                // Arrange
-                Dictionary<string, decimal> allocatedMoney = new Dictionary<string, decimal>
-            {
-                { "Category1", 30 },
-                { "Category2", 40 },
-                { "Category3", 20 }
-            };
+//            [TestMethod]
+//            public void TestIfAllocationMoney()
+//            {
+//                // Arrange
+//                Dictionary<string, decimal> allocatedMoney = new Dictionary<string, decimal>
+//            {
+//                { "Category1", 30 },
+//                { "Category2", 40 },
+//                { "Category3", 20 }
+//            };
 
-                decimal availableMoney = 100;
 
-                // Act
-                bool result = IsAllocationMoneySufficient(allocatedMoney, availableMoney);
+//                decimal availableMoney = 100;
 
-                // Assert
-                Assert.IsTrue(result);
-            }
+//                // Act
+//                bool result = IsAllocationMoneySufficient(allocatedMoney, availableMoney);
 
-            public bool TotalGreaterThanThreshold(List<decimal> donations, decimal threshold)
-            {
-                decimal totalDonation = donations.Sum();
-                return totalDonation > threshold;
-            }
+//                // Assert
+//                Assert.IsTrue(result);
+//            }
 
-            public bool GoodsSufficient(Dictionary<string, decimal> goodsAvailable, decimal threshold)
-            {
-                decimal totalGoods = goodsAvailable.Values.Sum();
-                return totalGoods > threshold;
-            }
+//            public bool TotalGreaterThanThreshold(List<decimal> donations, decimal threshold)
+//            {
+//                decimal totalDonation = donations.Sum();
+//                return totalDonation > threshold;
+//            }
 
-            public bool IsAllocationMoneySufficient(Dictionary<string, decimal> allocatedMoney, decimal availableMoney)
-            {
-                decimal totalAllocatedMoney = allocatedMoney.Values.Sum();
-                return totalAllocatedMoney <= availableMoney;
-            }
-        }
-    }
-}
+//            public bool GoodsSufficient(Dictionary<string, decimal> goodsAvailable, decimal threshold)
+//            {
+//                decimal totalGoods = goodsAvailable.Values.Sum();
+//                return totalGoods > threshold;
+//            }
+
+//            public bool IsAllocationMoneySufficient(Dictionary<string, decimal> allocatedMoney, decimal availableMoney)
+//            {
+//                decimal totalAllocatedMoney = allocatedMoney.Values.Sum();
+//                return totalAllocatedMoney <= availableMoney;
+//            }
+//        }
+//    }
+//}
